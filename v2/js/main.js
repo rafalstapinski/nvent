96c944367c4b4bb997f11cbe3052545da3a7998c
jonahJS <i@nvent.xyz>
2016-10-03 12:53:25 -0400

    finished smooth scrolling

---
+++ v2/js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+  //WINDOW SCROLLING STUFF HERE
+
+  // set variables here
+  var scrollPos = $(window).scrollTop();
+
+  //Get position of scroll
+  var getScrollPos = function() {
+    scrollPos = $(window).scrollTop();
+    return scrollPos;
+  }
+
+  // do stuff when scrolling
+  var ifScrolling = function(pos) {
+    if (pos === 0) {
+      $(".menu_wrap").css("background-color", "transparent");
+    } else {
+      $(".menu_wrap").css("background-color", "#FFFFFF");
+    }
+  }
+
+  // On load and scroll:
+  ifScrolling(getScrollPos());
+  $(window).scroll(function() {
+    ifScrolling(getScrollPos());
+  });
+
+
+  // find ID of this shit
+  $(function() {
+    var smoothScroll = function(el) {
+      var anchor = el.attr('id');
+      console.log(anchor);
+      var anchorLink = anchor.split("_");
+      var targetId = anchorLink[1];
+      console.log(targetId);
+      var target = document.getElementById(targetId);
+      console.log(target);
+      var targetPos = target.offsetTop;
+
+      console.log(targetPos);
+      $('html, body').animate({
+          scrollTop: $(target).offset().top
+        }, 1000);
+    }
+
+    $("#menu_list > li").click(function() {
+      var elem = $( this );
+      console.log(elem);
+      smoothScroll(elem);
+    });
+
+    $(".logo > h1").click(function() {
+      var elem = $( this );
+      smoothScroll(elem);
+    });
+
+    $(".button, .link_button, .link").click(function() {
+      var elem = $( this );
+      console.log(elem);
+      smoothScroll(elem);
+    })
+  });
+
+});
